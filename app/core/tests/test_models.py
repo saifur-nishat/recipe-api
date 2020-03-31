0c66c9ab08522ae24ef085143ca6fa24ded8068d
saifur-nishat <saifur.nishat@gmail.com>
2020-03-31 22:22:06 +0600

    Updated admin to support custom user model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,7 +6,7 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
-        email = "saifur.nishat@gmail.com"
+        email = "test@test.com"
         password = 'Testpass123'
         user = get_user_model().objects.create_user(
             email=email,
@@ -18,7 +18,7 @@ class ModelTests(TestCase):
 
     def test_new_user_email_normalized(self):
         """Test the email  for a new user is normalized"""
-        email = 'saifur.nishat@GMAIL.COM'
+        email = 'test@TEST.COM'
         user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
@@ -31,7 +31,7 @@ class ModelTests(TestCase):
     def test_create_new_superuser(self):
         """Test creating a new superuser"""
         user = get_user_model().objects.create_superuser(
-            'saifur.nishat@gmail.com',
+            'test@test.com',
             'test123'
         )
 
